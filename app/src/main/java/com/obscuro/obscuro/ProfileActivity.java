9394e3e89f10ded46dea21a736095e1fd9e8e6d6
Jacob Kim <jkim3213@gatech.edu>
2017-10-28 21:55:20 -0400

    add google json

--- app/src/main/java/com/obscuro/obscuro/ProfileActivity.java
+++ app/src/main/java/com/obscuro/obscuro/ProfileActivity.java
@@ -1,13 +1,37 @@
 package com.obscuro.obscuro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    EditText tag;
+    TextView loggedInAs, logBox;
+    Button ping, logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        tag = (EditText)findViewById(R.id.tagOne);
+        loggedInAs = (TextView)findViewById((R.id.loggedInAs));
+        logBox = (TextView)findViewById((R.id.logBox));
+        ping = (Button)findViewById((R.id.pingButton));
+        logout = (Button)findViewById((R.id.logout));
+
+    }
+
+    public void onLogout(View v){
+        startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
+    }
+
+    public void onPing(View v){
+
     }
 }
